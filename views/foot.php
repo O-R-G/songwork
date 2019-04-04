57a954e009ecdb442317b003c8e66998130a703e
Eric Li <ericyoungli@gmail.com>
2019-04-04 16:23:32 -0400

    update going back on lozenge for firefox

--- views/foot.php
+++ views/foot.php
@@ -1,10 +1,9 @@
+	<script>
+		var links = document.links;
+		for (var i = 0; i < links.length; i++) {
+			if (!links[i].classList.contains('no-blank') && links[i].hostname != location.hostname)
+		     links[i].target = "_blank";
+		}
+	</script>
 	</body>
 </html>
-
-<script>
-	var links = document.links;
-	for (var i = 0; i < links.length; i++) {
-		if (links[i].hostname != location.hostname)
-	     links[i].target = "_blank";
-	}
-</script>
