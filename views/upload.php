feb573e84cdce72feaddba6bed2e1bdc20951e7f
David Reinfurt <reinfurt@o-r-g.com>
2020-07-10 14:37:52 +0000

    adjust upload and lib

--- views/upload.php
+++ views/upload.php
@@ -87,9 +87,11 @@ if(isset($recordings_id)){
 		// execute bash code
 		$old_path = getcwd();
 		chdir('/var/www/app/songworks/_make/');
-		$output = shell_exec('./__make.sh');
+		// $output = shell_exec('./__make.sh');
+		$output = shell_exec('sudo ./__make.sh 2>&1');
+		//$output = shell_exec('./__test.sh 2>&1');
+		//$output2 = shell_exec('echo $JAVA_HOME 2>&1');
 		chdir($old_path);
-		var_dump($output);
 		echo "<pre>".$output."</pre>";
 	}
 	
