d7f1b43842df68c62b855f2754183454fb90ba80
Wei-Hao Wang <wwang03@risd.edu>
2020-09-14 10:40:31 -0400

    move bash code back to uplaod.php

--- views/upload-response.php
+++ views/upload-response.php
@@ -1,14 +1,5 @@
 <?
 	if($uri[2] == 'success'){
-		// execute bash code
-		$old_path = getcwd();
-		chdir('/var/www/app/songwork-app/_make/');
-		// $output = shell_exec('./__make.sh');
-		$output = shell_exec('./__make.sh 2>&1');
-		//$output = shell_exec('./__test.sh 2>&1');
-		//$output2 = shell_exec('echo $JAVA_HOME 2>&1');
-		chdir($old_path);
-		echo "<pre>".$output."</pre>";
 		
 		$column_left = 'Thanks for contributing to Songwork.org.';
 	}
