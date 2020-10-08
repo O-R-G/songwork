27f9db9773a89f2f6b01782c2e569e541a4d97ce
Wei-Hao Wang <wwang03@risd.edu>
2020-10-08 21:24:32 +0800

    run processing at the background

--- views/upload-response.php
+++ views/upload-response.php
@@ -3,7 +3,7 @@
 		// execute bash code
 		$old_path = getcwd();
 		chdir('/var/www/app/songwork-app/_make/');
-		$output = shell_exec('./__make.sh 1>>debug.log 2>&1');
+		$output = shell_exec('./__make.sh 1>>debug.log 2>&1 &');
 		chdir($old_path);
 		echo "<pre>".$output."</pre>";
 		$column_left = 'Thanks for contributing to Songwork.org.';
