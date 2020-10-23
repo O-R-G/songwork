ec1ad137a62b453f7c0fa9a5aaa3552c5aaa44ee
Wei-Hao Wang <wwang03@risd.edu>
2020-10-23 11:15:56 +0800

    pass audio filename to _make.sh

--- views/submit-response.php
+++ views/submit-response.php
@@ -3,6 +3,7 @@
 	$column_left = $item['deck'];
 	if($uri[2] == 'success'){
 		$audio_filename = $_POST['audio_filename'];
+		$audio_filename = str_replace("", "-", $_POST['audio_filename']);
 		$old_path = getcwd();
 		chdir('/var/www/app/songwork-app/_make/');
 		$output = shell_exec("./__make.sh ".$audio_filename." 1>>debug.log 2>&1 &");
